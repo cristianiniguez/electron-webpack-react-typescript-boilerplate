e550571a7f47c20bd94a03f58584317ed4d6b116
cristianiniguez <felizpc1@gmail.com>
2021-01-13 20:33:56 -0400

    react dev tools installed

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,23 +1,41 @@
 import { app, BrowserWindow } from 'electron';
-declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
+import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
+declare const MAIN_WINDOW_WEBPACK_ENTRY: any; // eslint-disable-line
+
+const dev: boolean = process.env.NODE_ENV === 'development';
+let mainWindow: BrowserWindow | null;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (require('electron-squirrel-startup')) {
+  // eslint-disable-line global-require
   app.quit();
 }
 
 const createWindow = (): void => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
+  });
+
+  app.whenReady().then(() => {
+    if (dev) {
+      installExtension(REACT_DEVELOPER_TOOLS)
+        .then((name) => {
+          console.log(`Added Extension: ${name}`);
+          // Open the DevTools.
+          mainWindow.webContents.openDevTools();
+        })
+        .catch((error) => console.log(`An error occurred: ${error}`));
+    }
   });
 
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
